64cfe07975368d4b5d84c66c8f5290e2ede097d7
bdot100 <bidemiokunade@gmail.com>
2023-08-07 09:43:56 +0100

    concat function

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -6,7 +6,7 @@ n as argument and returns the floor of the float.
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """type-annotated function floor which takes a float n
     as argument and returns the floor of the float."""
     return math.floor(n)
