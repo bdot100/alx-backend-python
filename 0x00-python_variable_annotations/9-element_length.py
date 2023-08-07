76b2f86d5c25b16951e3567b09bcbac34a9ad756
bdot100 <bidemiokunade@gmail.com>
2023-08-07 11:40:53 +0100

    Return Callable function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """
-Write a type-annotated function make_multiplier that takes
-a float multiplier as argument and returns a function that
-multiplies a float by multiplier.
+Annotate the below function’s parameters and return
+values with the appropriate types
+
+
 """
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    Write a type-annotated function make_multiplier that takes a
-    float multiplier as argument and returns a function that
+    Write a type-annotated function make_multiplier that takes a 
+    float multiplier as argument and returns a function that 
     multiplies a float by multiplier.
     """
     def multiplier_function(x: float) -> float:
