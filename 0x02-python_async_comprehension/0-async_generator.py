a5a807c97fc45241805c275d48b2fdda0406e72c
bdot100 <bidemiokunade@gmail.com>
2023-08-09 20:32:21 +0100

    Create an Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+a coroutine called async_generator that takes no arguments.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """a coroutine called async_generator that takes no arguments."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
