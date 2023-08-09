7551bb98a201180ccc8fc79706ad594d3c4704ee
bdot100 <bidemiokunade@gmail.com>
2023-08-09 19:29:21 +0100

    Return Task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module is a regular function that takes an integer
+max_delay and returns a asyncio.task
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+    This function returns an asyncio.Task
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
